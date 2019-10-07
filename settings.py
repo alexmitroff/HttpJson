889221245c182efdcbc6965c77ff1394d6f94f31
alexmitroff <alexmitroff@gmail.com>
2019-10-07 12:08:55 +0300

    Update settings

--- settings.py
+++ settings.py
@@ -1,9 +1,12 @@
-# Python server self address
-SERVER_ADDRESS = '192.168.0.104'
+# Python server self address:
+SERVER_ADDRESS = '0.0.0.0'
 SERVER_PORT = 8000
 
-# Send UDP datagram
-CLIENT_ADDRESS = '192.168.0.102'
+# Send UDP datagram to:
+CLIENT_ADDRESS = '192.168.90.102'
 CLIENT_PORT = 6625
+
+SEND_DATAGRAM_KEY = 'q'
+STOP_KEYLISTENING_KEY = 'p'
 
 JSON_FILE_PATH = './example.json'
